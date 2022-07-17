3d6b6f7e64e41216939670157f8f1407b8fd39f7
vpl3ntz <vitorribasplentz@gmail.com>
2022-07-16 22:19:43 -0300

    front-end

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,9 @@
-import React from 'react';
+import React, { useEffect} from 'react';
+import api from './services/api';
 
 function App() {
+  useEffect(() => {}, []);
+  
   return (
     <div className="App">
 
